cdc9868dac0f3ffa5b2964657ce286fa144fcb8c
Paulo Paupitz Neto <paupitzneto@gmail.com>
2025-06-03 15:00:15 -0300

    criação scripts e pequenas mudanças só

---
+++ CRUDS/js/login.js
@@ -0,0 +1,11 @@
+function logarUsuario(){
+    
+    let email = document.getElementById("txtEmail")
+    let pw = documento.getElementById("pwd")
+
+}
+
+const toggleLoadingIcon = () => {
+    document.getElementById("enterButton").style.display = document.getElementById("enterButton").style.display != "none" ? "none" : "block";
+    document.getElementById("loadingIcon").style.display = document.getElementById("loadingIcon").style.display == "block" ? "none" : "block";
+  };
---
+++ CRUDS/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const cadastroButton = document.getElementById("cadastroButton");
+    const popup = document.getElementById("popup");
+    const btnSim = document.getElementById("btnSim");
+    const btnCancelar = document.getElementById("btnCancelar");
+  
+   
+  
+   });
+  
