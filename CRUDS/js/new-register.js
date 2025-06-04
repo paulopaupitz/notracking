f78d317ab5bb8616549854b9cf54814a5893b715
Paulo Paupitz Neto <paupitzneto@gmail.com>
2025-06-04 14:56:53 -0300

    modificacao header, criação começo js, add imagens

---
+++ CRUDS/js/new-register.js
@@ -0,0 +1,27 @@
+document.getElementById("registroButton").addEventListener("click",function() {
+
+    const nome = document.getElementById("txtNome").value;
+    const sobrenome = document.getElementById("txtNome").value;
+    const ativo = document.getElementById("txtNome").value;
+    const dtNasc = document.getElementById("txtNome").value;
+    const email = document.getElementById("txtNome").value;
+    const endereco = document.getElementById("txtNome").value;
+    const outrasInfos = document.getElementById("txtNome").value;
+    const interesses = document.getElementById("txtNome").value;
+    const sentimentos = document.getElementById("txtNome").value;
+    const valores = document.getElementById("txtNome").value;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
