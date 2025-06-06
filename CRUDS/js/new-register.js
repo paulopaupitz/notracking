4650803e075822ba044d065752673e2ebdb6e27c
Paulo Paupitz Neto <paupitzneto@gmail.com>
2025-06-06 14:10:31 -0300

    alterar styles

--- CRUDS/js/new-register.js
+++ CRUDS/js/new-register.js
@@ -1,15 +1,30 @@
 document.getElementById("registroButton").addEventListener("click",function() {
 
     const nome = document.getElementById("txtNome").value;
-    const sobrenome = document.getElementById("txtNome").value;
-    const ativo = document.getElementById("txtNome").value;
-    const dtNasc = document.getElementById("txtNome").value;
-    const email = document.getElementById("txtNome").value;
-    const endereco = document.getElementById("txtNome").value;
-    const outrasInfos = document.getElementById("txtNome").value;
-    const interesses = document.getElementById("txtNome").value;
-    const sentimentos = document.getElementById("txtNome").value;
-    const valores = document.getElementById("txtNome").value;
+    const sobrenome = document.getElementById("txtSobrenome").value;
+    const dtNasc = document.getElementById("dataNascimento").value;
+    const email = document.getElementById("txtEmail").value; 
+    const endereco = document.getElementById("txtEndereco").value; 
+    const outrasInfos = document.getElementById("txtInfo").value; 
+    const interesses = document.getElementById("txtInteresse").value; 
+    const sentimentos = document.getElementById("txtSentimentos").value; 
+    const valores = document.getElementById("txtValores").value; 
+
+    const novoCliente = { 
+        nome, sobrenome, dtNasc, email, endereco, outrasInfos, interesses, sentimentos, valores 
+    }; 
+    
+    let clientes = JSON.parse(localStorage.getItem('clientes')) || [];
+
+    clientes.push(novoCliente);
+
+    localStorage.setItem('clientes', JSON.stringify(clientes));
+
+    const form = document.getElementById("form-register");
+
+    if (form)
+        form.reset();
+    
     
 
 
