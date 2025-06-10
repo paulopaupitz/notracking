700e95210e2f0a1029671772f3c7772ea1e409d8
Paulo Paupitz Neto <paupitzneto@gmail.com>
2025-06-10 14:58:35 -0300

    arrumado alguns estilos, htmls cadastro cliente

--- CRUDS/js/script.js
+++ CRUDS/js/script.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cadastroButton = document.getElementById("cadastroButton");
-    const popup = document.getElementById("popup");
-    const btnSim = document.getElementById("btnSim");
-    const btnCancelar = document.getElementById("btnCancelar");
+    
   
    
   
